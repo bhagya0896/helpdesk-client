58eb10c0746893cf8a286c395cce59bcdbac0c4b
bhagya0896 <77100278+bhagya0896@users.noreply.github.com>
2021-05-23 22:39:38 +0530

    Update interaction.js

--- src/tickets/interaction.js
+++ src/tickets/interaction.js
@@ -1,6 +1,6 @@
  import axios from 'axios';
 
- const BASE_URL = 'http://localhost:5000/user';
+ const BASE_URL = ' https://helpdeskticket-server.herokuapp.com/user';
 
  //getTickets
 export const fetchTickets =(token)=>{
