834abb9bb18497a8efc1391bcd8e81e04069afe8
bhagya0896 <77100278+bhagya0896@users.noreply.github.com>
2021-05-23 22:36:59 +0530

    Update register.js

--- src/components/register.js
+++ src/components/register.js
@@ -44,7 +44,7 @@ function Register ()
         else{
             setcustomMessage("")
             return axios
-            .post(`http://localhost:5000/user/register`,{firstname,lastname,email,password})
+            .post(` https://helpdeskticket-server.herokuapp.com/user/register`,{firstname,lastname,email,password})
             .then((res)=>
             {
                 let data = res.data;
